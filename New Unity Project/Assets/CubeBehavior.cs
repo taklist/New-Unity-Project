06e76522938a8e332434d4de5c738819423902c1
taklist <bjorn.janson.94@gmail.com>
2016-07-03 23:32:48 +0200

    first commit

---
+++ New Unity Project/Assets/CubeBehavior.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveByKeys();
+	}
+
+	void MoveByKeys() {
+		if (Input.GetKey(KeyCode.DownArrow)){
+			transform.Translate (0f, 0f, -1f * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.UpArrow)){
+			transform.Translate (0f, 0f, 1f * Time.deltaTime);
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow)){
+			transform.Translate (1f * Time.deltaTime, 0f, 0f);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			transform.Translate (-1f * Time.deltaTime, 0f, 0f);
+		}
+	}
+}
